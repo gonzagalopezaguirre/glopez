98177bfccd590c71fc0cc2572043ed7ba1384a27
glopez <glopez@106-23.info2>
2012-05-21 13:25:41 +0200

    Este es el commit de los métodos metodo1_clase1 y metodo1_clase2 previo a la primera rama.

--- glopez/src/clase1.java
+++ glopez/src/clase1.java
@@ -1,5 +1,9 @@
 public class clase1{
 
+	public static void metodo1_clase1(){
+		System.out.println("Soy el método 1 de la clase 1");
+	}
+
 	public static void main(String[] args){
 		System.out.println("Soy la clase 1");
 	}
--- glopez/src/clase2.java
+++ glopez/src/clase2.java
@@ -1,5 +1,9 @@
 public class clase2{
 
+	public static void metodo1_clase2(){
+		System.out.println("Soy el método 1 de la clase 2");
+	}
+
 	public static void main(String[] args){
 		System.out.println("Soy la clase 2");
 	}
