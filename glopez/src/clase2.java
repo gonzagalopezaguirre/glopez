de0a442e99fdf5bb6cce668c01f3088cb0eddb9b
glopez <glopez@106-23.info2>
2012-05-21 13:34:39 +0200

    Este es el commit del método rama master de la clase 2.

--- glopez/src/clase2.java
+++ glopez/src/clase2.java
@@ -4,6 +4,10 @@ public class clase2{
 		System.out.println("Soy el método 1 de la clase 2");
 	}
 
+	public static void metodo_rama_master_clase2(){
+		System.out.println("Soy el método de la rama master");
+	}
+
 	public static void main(String[] args){
 		System.out.println("Soy la clase 2");
 	}
